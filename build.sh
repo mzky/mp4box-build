e4c71c4c48807d8a3095ad00c452bf768a48c292
池边树下 <mzky@163.com>
2024-11-22 15:21:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+# https://github.com/gpac/gpac/wiki/GPAC-build-MP4Box-only-all-platforms
 apt install mingw-w64 gcc g++ make git automake zlib1g-dev freeglut3 freeglut3-dev build-essential fakeroot dpkg-dev devscripts ccache debhelper pkg-config mesa-utils lcov libfreetype6-dev libjpeg62-dev libpng-dev libmad0-dev libfaad-dev libogg-dev libvorbis-dev libtheora-dev liba52-0.7.4-dev libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libavdevice-dev libxv-dev x11proto-video-dev libgl1-mesa-dev x11proto-gl-dev libxvidcore-dev libssl-dev libjack-dev libasound2-dev libpulse-dev libsdl2-dev dvb-apps 
 git clone https://github.com/gpac/gpac.git --depth=1 
 cd gpac
